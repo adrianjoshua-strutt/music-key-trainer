ec38d938a2a219afaf168d0cf4606f2e897661ac
Adrian Joshua Strutt <adrianjoshua.strutt@gmail.com>
2024-02-22 16:12:38 +0100

    update dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/music-key-trainer/'
-      : '/'
+      : '/',
+    configureWebpack: {
+        module: {
+            rules: [
+                { 
+                    test: /\.mid$/, type: 'asset/source' 
+                },
+            ],
+        },
+    }
   }
   
